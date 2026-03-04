34174163966d6affc99bbdd9fba3b6cff64d3ced
Rónai Márton János <ronai.marton.janos@diak.szbi-pg.hu>
2026-03-04 08:58:41 +0100

    commentek hozzaadasa

--- auto.py
+++ auto.py
@@ -1,19 +1,31 @@
+#ez gyartya le magat a classt
 class Auto:
+    #minden fuggvenybe be kell irni a self-et
+    #minden ertek ele be kell irni a self-et
+
+
+    #ez ruhazza fel a targyat tulajdonsagokkal
     def __init__(self, marka_, tipus_, gyartasi_ev_):
         self.marka = marka_
         self.tipus = tipus_
         self.gyartasi_ev = gyartasi_ev_
+        #az olyan erteket, aminek meg van adva aaz erteke, utoljara jonnek es nem kerulnek bele a fuggveny ()-be
         self.sebesseg = 0
 
+    #ez iratja ki a targy tulajdonsagait, ha kiprinteled
     def __str__(self):
         return f"{self.marka} {self.tipus} ({self.gyartasi_ev}), sebesség {self.sebesseg} km/h"
     
+    #ezzel oldjuk meg a 2. feladatot
     def gyorsit(self, ertek):
         self.sebesseg += ertek
+        #megadjuk, hogy maximum mennyivel mehet az auto
         if self.sebesseg > 200:
             self.sebesseg = 200
 
+    #ezzel oldjuk meg a 3. feladatot
     def fekez(self, ertek):
         self.sebesseg -= ertek
+        #megadjuk, hogy az auto nem mehet 0-nal kevesebbel
         if self.sebesseg < 0:
             self.sebesseg = 0
--- main.py
+++ main.py
@@ -1,13 +1,41 @@
-from auto import Auto
 
+#ezzel importaljuk at a classt ebbe a fajlba. az auto az auto.py fajl neve az Auto meg a class neve
+from auto import Auto
+import random
+
+#letrehozunk targyakat es megadjuk nekik az ertekeiket
 auto1 = Auto("Toyota", "Corolla", 2015)
 auto2 = Auto("Ford", "Focus", 2018)
+auto3 = Auto("Audi", "E-Tron GT", 2021)
+auto4 = Auto("Ford", "Mustang", 2020)
 
-print(auto1)
-print(auto2)
 
-auto1.gyorsit(150)
-print(auto1)
+# print(auto1)
+# print(auto2)
 
-auto1.fekez(100)
-print(auto1)
+# auto1.gyorsit(150)
+# print(auto1)
+
+# auto1.fekez(100)
+# print(auto1)
+
+#letrehozunk egy listat amibe belerakjuk az osszes autot
+autok = [auto1, auto2, auto3, auto4]
+for auto in autok:
+    print(auto)
+
+#megadunk minden autonak egy random gyorsitasi erteket
+print("\n Gyorsit\n")
+for auto in autok:
+    auto.gyorsit(random.randint(30, 150))
+    print(auto)
+
+#megadjuk az autok atlag eletszamat
+autok_szama = len(autok)
+ossz_eletkor = 0
+for auto in autok:
+    kor = 2026 - auto.gyartasi_ev
+    ossz_eletkor += kor
+
+atlag_eletkor = ossz_eletkor/autok_szama
+print(f"az autok atlagaeletkora: {atlag_eletkor} év")
