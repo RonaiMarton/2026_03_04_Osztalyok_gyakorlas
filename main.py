03434e21cac1b92380307ec768810012e6ecb2dc
Rónai Márton János <ronai.marton.janos@diak.szbi-pg.hu>
2026-03-04 08:37:09 +0100

    gyorsitas, fekezes

--- main.py
+++ main.py
@@ -2,5 +2,12 @@ from auto import Auto
 
 auto1 = Auto("Toyota", "Corolla", 2015)
 auto2 = Auto("Ford", "Focus", 2018)
+
 print(auto1)
 print(auto2)
+
+auto1.gyorsit(150)
+print(auto1)
+
+auto1.fekez(100)
+print(auto1)
